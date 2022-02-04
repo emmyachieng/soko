b9cca0b8f95e4d4fe6657bbd0bd63517ed0e66e1
emmyachieng <emmyachieng97@gmail.com>
2022-02-04 10:01:10 +0300

    added categories relationships within the context

--- lib/soko/catalog/product.ex
+++ lib/soko/catalog/product.ex
@@ -1,12 +1,15 @@
 defmodule Soko.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Soko.Catalog.Category
 
   schema "products" do
     field :description, :string
     field :price, :decimal
     field :title, :string
     field :views, :integer
+
+    many_to_many :categories, Category, join_through: "product_categories", on_replace: :delete
 
     timestamps()
   end
